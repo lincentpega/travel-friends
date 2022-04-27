64dbade16bb098ebbf2b1048f2a9ac584557ae0f
lincentpega <imkrasnyukov@gmail.com>
2022-04-27 13:00:53 +0300

    added login system (crashes)

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/profiles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -33,12 +36,50 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/profile", methods=['GET', 'POST'])
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/dating", methods=['GET', 'POST'])
+@login_required
+def dating():
+    persons_data_list = Profiles.query.all()
+    return render_template("dating.html", persons_list=persons_data_list, size=len(persons_data_list))
+
+
+@app.route("/about", methods=['POST'])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = Profiles.query.filter_by(login=login).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+
+            # next_page = request.args.get('next_page')
+            # redirect('next_page')
+            redirect('dating')
+        else:
+            flash('Неверный пароль!')
+    else:
+        flash('Введите логин и пароль!')
+        return render_template('login.html')
+
+
 @app.route("/registration", methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
+        password2 = request.form['password2']
         name = request.form['name']
         age = request.form['age']
         country = request.form['country']
@@ -50,35 +91,33 @@ def registration():
         phone = request.form['phone']
         photo = request.form['photo']
 
-        new_profile = Profiles(email=email, username=username, password=password, name=name, age=age, country=country,
-                               city=city, languages=languages, address=address,
-                               contacts=contacts, about=about_text, phone=phone,
-                               photo=photo)
+        if not(username or password or password2):
+            flash('Пожалуйста, заполните поля: Имя пользователя, Пароль, Повторите пароль')
+        elif password != password2:
+            flash('Пароли отличаются, повторите ввод!')
+        else:
+            pass_hash = generate_password_hash(password)
+            new_profile = Profiles(email=email, username=username, password=pass_hash, name=name, age=age,
+                                   country=country,
+                                   city=city, languages=languages, address=address,
+                                   contacts=contacts, about=about_text, phone=phone,
+                                   photo=photo)
+            try:
+                db.session.add(new_profile)
+                db.session.commit()
+                return redirect(url_for('login_page'))
+            except:
+                return "Ошибка при добавлении пользователя"
 
-        # try:
-        db.session.add(new_profile)
-        db.session.commit()
-        return redirect('/')
-        # except:
-        #     return "Ошибка при добавлении пользователя"
     else:
         return render_template("registration.html")
 
 
-@app.route("/profile", methods=['GET', 'POST'])
-def profile():
-    return render_template("profile.html")
-
-
-@app.route("/dating", methods=['GET', 'POST'])
-def dating():
-    persons_data_list = Profiles.query.all()
-    return render_template("dating.html", persons_list=persons_data_list, size=len(persons_data_list))
-
-
-@app.route("/about", methods=['GET', 'POST'])
-def about():
-    return render_template("about.html")
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    redirect(url_for('index'))
 
 
 if __name__ == "__main__":
